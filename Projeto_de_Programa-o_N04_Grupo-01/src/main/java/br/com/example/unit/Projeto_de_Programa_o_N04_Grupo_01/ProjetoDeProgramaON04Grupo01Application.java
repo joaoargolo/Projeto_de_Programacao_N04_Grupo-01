b0ad7783df1eaa466cfeb95a71063f1b01716d81
luiz <luiz.fsobral@souunit.com.br>
2025-10-13 16:46:33 -0300

    Primeiro commit

---
+++ Projeto_de_Programa-o_N04_Grupo-01/src/main/java/br/com/example/unit/Projeto_de_Programa_o_N04_Grupo_01/ProjetoDeProgramaON04Grupo01Application.java
@@ -0,0 +1,13 @@
+package br.com.example.unit.Projeto_de_Programa_o_N04_Grupo_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeProgramaON04Grupo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeProgramaON04Grupo01Application.class, args);
+	}
+
+}
