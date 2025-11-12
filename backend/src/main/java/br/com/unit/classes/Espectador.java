a947d99dc690324c88104635b37b40635f1d9ce2
Pedro Faria <168576911+PedroFariaBarreto@users.noreply.github.com>
2025-11-12 15:14:01 -0300

    modificação na calsse espectador

--- backend/src/main/java/br/com/unit/classes/Espectador.java
+++ backend/src/main/java/br/com/unit/classes/Espectador.java
@@ -3,6 +3,9 @@ package br.com.unit.classes;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,6 +18,27 @@ public class Espectador extends Pessoa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEspectador;
 
+    public enum Status {
+        ATIVO,
+        INATIVO,
+        PENDENTE_DE_CONFIRMACAO
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status = Status.INATIVO;
+
+    @ManyToMany(mappedBy = "espectadores")
+    private List<Evento> eventosDoEspectador;
+
+    public void atualizarStatus() {
+        if (eventosDoEspectador == null || eventosDoEspectador.isEmpty()) {
+            this.status = Status.INATIVO;
+        } else {
+            this.status = Status.ATIVO;
+        }
+    }
+
     public void cadastrarEvento() {
         System.out.println("O usuário foi cadastrado no evento.");
     }
@@ -22,4 +46,11 @@ public class Espectador extends Pessoa {
     public void sairEvento() {
         System.out.println("O usuário foi retirado do evento");
     }
+
+    @Column(name = "reset_token")
+    private String resetToken;
+
+    @Column(name = "token_expiration")
+    private LocalDateTime tokenExpiration;
+
 }
